b3b888d0db00418f88d99d2c959c32655089d592
Viliami Fihaki <41652232+vfihaki@users.noreply.github.com>
2019-03-28 09:33:34 +1300

    first commit

---
+++ Q1.Book/Q1.Book/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1.Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book newbook = new Book();
+
+            Console.WriteLine("*** ENTER NEW BOOK DETAILS BELOW ***\n");
+
+            Console.Write("Enter Book Title: ");
+            newbook.SetTitle(Console.ReadLine());
+            Console.Write("Enter Book Author: ");
+            newbook.SetAuthor(Console.ReadLine());
+            Console.Write("Enter Book Publisher: ");
+            newbook.SetPublisher(Console.ReadLine());
+
+            Console.WriteLine("\n******************************************");
+            Console.WriteLine($"The following new book has been processed:\n{newbook.GetTitle()}\n{newbook.GetAuthor()}\n{newbook.GetPublisher()}");
+
+            Console.ReadLine();
+        }
+    }
+    class Book
+    {
+        private string title;
+        private string author;
+        private string publisher;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+        public string GetTitle()
+        {
+            return $"Book Title: {Title}";
+        }
+        public void SetTitle(string tle)
+        {
+
+        }
+        public string GetAuthor()
+        {
+            return $"Author: {Author}";
+        }
+        public void SetAuthor(string tle)
+        {
+
+        }
+        public string GetPublisher()
+        {
+            return $"Publisher: {Publisher}";
+        }
+        public void SetPublisher(string tle)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Q3.UpdatedUML.Book/Q3.UpdatedUML.Book/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3.UpdatedUML.Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book newbook = new Book();
+
+          
+
+                Console.WriteLine("*** ENTER NEW BOOK DETAILS BELOW ***\n");
+
+                Console.Write("Enter Book Title: ");
+                string BookTitle = Console.ReadLine();
+                Console.Write("Enter Book Author: ");
+                string BookAuthor = Console.ReadLine();
+                Console.Write("Enter Book Publisher: ");
+                string BookPublisher = Console.ReadLine();
+
+
+                Book b1 = new Book(BookTitle, BookAuthor, BookPublisher);
+                newbook = b1;
+
+            
+                Console.WriteLine("\n******************************************");
+                Console.WriteLine(newbook.GetBookDetail());
+            
+            Console.ReadLine();
+        }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+
+        public Book()
+        {
+
+        }
+        public Book( string _title, string _author, string _publisher)
+        {
+            Title = _title;
+            Author = _author;
+            Publisher = _publisher;
+        }
+        public string GetBookDetail()
+        {
+            return $"The following new book has been processed:\nBook Title: {Title}\nAuthor: {Author}\nPublisher: {Publisher}";
+        }
+    }
+
+}
+
